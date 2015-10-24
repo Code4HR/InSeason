76c9cab601680b261faa8a8fdbc51d9937128e54
washt <washt23@gmail.com>
2015-10-24 11:22:14 -0400

    Added side bar toggle and ajax

--- Scripts/ajax.js
+++ Scripts/ajax.js
@@ -1,15 +1,36 @@
 $.getJSON('http://inseason.azurewebsites.net/animals', function (data) {
     $.each(data, function (key, value) {
+        var val = value.key
         if (value.key === "pheasant") {
-            $("#animalList").append('<a class="mdl-navigation__link" href=""><span class="icon-turkey material-icons" style="font-size: 2.5em"></span>' + value.human + '</a>');
+            var btn = $('<a id="' + value.human +'" class="mdl-navigation__link""><span class="icon-turkey material-icons" style="font-size: 2.5em"></span>' + value.human + '</a>')
+            btn.click(function () {
+              console.log($(this).attr("id"));
+            });
+            $("#animalList").append(btn);
         } else if (value.key === "weasel") {
-            $("#animalList").append('<a class="mdl-navigation__link" href=""><span class="icon-muskrat material-icons" style="font-size: 2.5em"></span>' + value.human + '</a>');
+            var btn = $('<a id="' + value.human +'" class="mdl-navigation__link"><span class="icon-muskrat material-icons" style="font-size: 2.5em"></span>' + value.human + '</a>')
+            btn.click(function () {
+              console.log($(this).attr("id"));
+            });
+            $("#animalList").append(btn);
         } else if (value.key === "raccoon") {
-            $("#animalList").append('<a class="mdl-navigation__link" href=""><span class="icon-racoon material-icons" style="font-size: 2.5em"></span>' + value.human + '</a>');
+            var btn = $('<a id="' + value.human +'" class="mdl-navigation__link" "><span class="icon-racoon material-icons" style="font-size: 2.5em"></span>' + value.human + '</a>')
+            btn.click(function () {
+              console.log($(this).attr("id"));
+            });
+            $("#animalList").append(btn);
         } else if (value.key === "mink") {
-            $("#animalList").append('<a class="mdl-navigation__link" href=""><span class="icon-muskrat material-icons" style="font-size: 2.5em"></span>' + value.human + '</a>');
+            var btn = $('<a id="' + value.human +'" class="mdl-navigation__link" "><span class="icon-muskrat material-icons" style="font-size: 2.5em"></span>' + value.human + '</a>')
+            btn.click(function () {
+              console.log($(this).attr("id"));
+            });
+            $("#animalList").append(btn);
         } else {
-            $("#animalList").append('<a class="mdl-navigation__link" href=""><span class="icon-' + value.key + ' material-icons" style="font-size: 2.5em"></span>' + value.human + '</a>');
+            var btn = $('<a  id="' + value.human +'" class="mdl-navigation__link" onClick="toggle(val)"><span class="icon-' + value.key + ' material-icons" style="font-size: 2.5em"></span>' + value.human + '</a>')
+            btn.click(function () {
+              console.log($(this).attr("id"));
+            });
+            $("#animalList").append(btn);
         }
     });
 });
