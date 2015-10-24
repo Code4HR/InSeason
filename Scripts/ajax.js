5bc4ac32d5bbd9077263e4d0adc5d8b36bc1bc57
washt <washt23@gmail.com>
2015-10-23 21:03:32 -0400

    Fixed animal population on sidebar

--- Scripts/ajax.js
+++ Scripts/ajax.js
@@ -1,6 +1,5 @@
 $.getJSON('http://inseason.azurewebsites.net/animals', function(data) {
        $.each(data, function(key,value) {
-        //  console.log(value);
-         $("body").append(<p>value</p>);
+         $("#animalList").append('<a class="mdl-navigation__link" href=""><i class="mdl-color-text--blue-grey-400 material-icons" role="presentation">home</i>' + value.human + '</a>');
        });
    });
