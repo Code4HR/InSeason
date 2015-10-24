0dc647b502ce59430df0722d5a1141e5bf919964
Benjamin Earley <bearley@outsitenetworks.com>
2015-10-24 10:32:42 -0400

    fixed animal icons

--- Scripts/ajax.js
+++ Scripts/ajax.js
@@ -1,6 +1,15 @@
-$.getJSON('http://inseason.azurewebsites.net/animals', function(data) {
-       $.each(data, function(key,value) {
-           $("#animalList").append('<a class="mdl-navigation__link" href=""><span class="icon-' + value.key + ' material-icons" style="font-size: 2.5em"></span>'
-                                    + value.human + '</a>');
-       });
-   });
+$.getJSON('http://inseason.azurewebsites.net/animals', function (data) {
+    $.each(data, function (key, value) {
+        if (value.key === "pheasant") {
+            $("#animalList").append('<a class="mdl-navigation__link" href=""><span class="icon-turkey material-icons" style="font-size: 2.5em"></span>' + value.human + '</a>');
+        } else if (value.key === "weasel") {
+            $("#animalList").append('<a class="mdl-navigation__link" href=""><span class="icon-muskrat material-icons" style="font-size: 2.5em"></span>' + value.human + '</a>');
+        } else if (value.key === "raccoon") {
+            $("#animalList").append('<a class="mdl-navigation__link" href=""><span class="icon-racoon material-icons" style="font-size: 2.5em"></span>' + value.human + '</a>');
+        } else if (value.key === "mink") {
+            $("#animalList").append('<a class="mdl-navigation__link" href=""><span class="icon-muskrat material-icons" style="font-size: 2.5em"></span>' + value.human + '</a>');
+        } else {
+            $("#animalList").append('<a class="mdl-navigation__link" href=""><span class="icon-' + value.key + ' material-icons" style="font-size: 2.5em"></span>' + value.human + '</a>');
+        }
+    });
+});
