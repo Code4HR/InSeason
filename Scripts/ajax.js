2d3a12369ffdf09b8818d1227ad5aa1501192324
Benjamin Earley <BenjaminEarley92@gmail.com>
2015-10-24 00:55:52 -0400

    small fixes

--- Scripts/ajax.js
+++ Scripts/ajax.js
@@ -1,6 +1,6 @@
 $.getJSON('http://inseason.azurewebsites.net/animals', function(data) {
        $.each(data, function(key,value) {
-           $("#animalList").append('<a class="mdl-navigation__link" href=""><span class="icon-' + value.human + ' material-icons"></span>'
+           $("#animalList").append('<a class="mdl-navigation__link" href=""><span class="icon-' + value.human + ' material-icons" style="font-size: 2.5em"></span>'
                                     + value.human + '</a>');
        });
    });
